4dc6476de6e47e08326ddded482cf077c97794cc
Caden <36030084+cgobat@users.noreply.github.com>
2021-01-24 22:09:05 -0800

    Code assignment completion

--- homework1/springforce.c
+++ homework1/springforce.c
@@ -1,26 +1,28 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#define PI 4.*atan(1) // simple way to use math library function to define pi
 
-void main()
+void main(int argc, char *argv[])
 {
-    double A_x = 2.;
-    double A_y = 1.;
-    double phi_x = 0.;
-    double phi_y = phi_x + M_PI_4;
-    double k = 1.0;
-    double m = 1.0;
+    if (argc != 5) {
+        printf("\nError: wrong number of input values provided.\nPlease pass 4 arguments: A_x, A_y, phi_x, and phi_y, separated by spaces.\n");
+    }
+
+    double A_x = atof(argv[1]);
+    double A_y = atof(argv[2]);
+    double phi_x = atof(argv[3]);
+    double phi_y = atof(argv[4]);
+    double k = 1.0; // set these to be whatever is desired
+    double m = 1.0; // for simplicity, I take them to be 1
     double w = sqrt(k/m);
     double wt;
 
-    //printf("Enter values for A_x, A_y, phi_x, and phi_y separated by commas: ");
-    //scanf("%lf,%lf,%lf,%lf",&A_x,&A_y,&phi_x,&phi_y);
-    //printf("%9s\t%9s\t%9s\n","t","x","y");
-
-    for(wt=0.; wt<=2.*M_PI; wt=wt+(2.*M_PI/100.))
+    for(wt=0.; wt<=2.*PI; wt=wt+(2.*PI/100.))
     {
-        double t = wt/w;
+        double t = wt/w; // actual time can be calculated because we set/know omega
         double x = A_x*sin(wt+phi_x);
         double y = A_y*sin(wt+phi_y);
-        printf("%9lf\t%9lf\t%9lf\n",t,x,y);
+        printf("%9lf\t%9lf\t%9lf\n",t,x,y); // print out time and corresponding x, y position
     }
 }
